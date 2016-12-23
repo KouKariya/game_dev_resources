c2ecce459307b800a8644d8b4353279abe508857
KouKariya <jssgallarzo@gmail.com>
2016-12-23 00:51:14 -0600

    Added shapes.py

--- hello.py
+++ hello.py
@@ -9,7 +9,11 @@ window = pygame.display.set_mode((500, 400))  # <--Tuple of numbers
 while True:
     # creates a rectangle, (255,0,0)= RGB color scheme
     # 2nd set of colors(x coor, y coor, width of rect, height of rect)
-    pygame.draw.rect(window, (255, 0, 0), (100, 100, 50, 50))
-    pygame.draw.rect(window, (0, 255, 0), (150, 100, 50, 50))
-    pygame.draw.rect(window, (0, 0, 255), (200, 100, 50, 50))
+    # pygame.draw.rect(window, (255, 0, 0), (100, 100, 50, 50))
+    # pygame.draw.rect(window, (0, 255, 0), (150, 100, 50, 50))
+    # pygame.draw.rect(window, (0, 0, 255), (200, 100, 50, 50))
+
+    # draw circle, (RBG),(location/point),radius,line-width
+    pygame.draw.circle(window, (255, 255, 0), (250, 200), 20, 0)
+
     pygame.display.update()
---
+++ shapes.py
@@ -0,0 +1,20 @@
+import pygame
+
+pygame.init()
+
+window = pygame.display.set_mode((500, 400))
+
+while True:
+    #Shows hollow rectangles with ellipses inside of them
+    pygame.draw.rect(window, (255, 0, 0), (100, 100, 100, 50), 2)
+    pygame.draw.ellipse(window, (255, 0, 0), (100, 100, 100, 50))
+    #
+    pygame.draw.rect(window, (0, 255, 0), (100, 150, 84, 40), 2)
+    pygame.draw.ellipse(window, (0, 255, 0), (100, 150, 80, 40))
+    #
+    pygame.draw.rect(window, (0, 0, 255), (100, 190, 60, 30), 2)
+    pygame.draw.ellipse(window, (0, 0, 255), (100, 190, 60, 30))
+    # Circle made with ellipse
+    pygame.draw.ellipse(window, (0, 0, 255), (100, 250, 40, 40))
+
+    pygame.display.update()
